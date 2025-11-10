f42fddefd6891eee420ef9b05711743362b6dc3f
Milena <milenatymofiichuk@gmail.com>
2025-11-09 21:00:16 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+let startButton = document.getElementById("startButton");
+
+const fonts = [
+  "Barriecito",
+  "Runik Puddles",
+  "Pirata One",
+  "Peralta",
+  "DynaPuff",
+  "Rubik Bubbles",
+  "Snowburst One",
+  "Matemasie",
+  "Ole",
+  "Rubik Vinyl",
+  "Ribeye Marrow",
+  "Workbench",
+  "Bungee Outline",
+  "Rye",
+  "Kings",
+];
+
+const colors = [
+  "#FFA4A4",
+  "#BADFDB",
+  "#F8FAB4",
+  "#FFDBB6",
+  "#CADCAE",
+  "#C1C8E4",
+  "#D7B2FF",
+  "#FFC3DE",
+  "#BFFCC6",
+  "#FFFFBA",
+  "#FF9CEE",
+  "#9AFFC2",
+  "#A0E7E5",
+  "#FFAEBC",
+  "#D0F4DE",
+  "#FBC4AB",
+];
